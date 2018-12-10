0eb006e93520bcae9d0b3deea382b37e1452065d
nabang97 <parawitayolanda@gmail.com>
2018-12-10 17:30:46 +0700

    perbaikan 1

---
+++ Home.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="responsive.css">
+    <title></title>
+  </head>
+  <body class="ukm-body">
+
+    <div class="ukm-container">
+      <div class="ukm-layer"></div>
+      <div class="ukm-content">
+          <img src="image/logo.png" alt="" class="ukm-logo">
+          <div class="ukm-title">
+            <h3>coming soon</h3>
+          </div>
+          <div class="ukm-grid">
+            <div class="ukm-fashion">
+              <h2 class="ukm-title">Fashion.</h2>
+              <p>Yang akan menjadi fokus kami dibidang fashion adalah pedagang hijab.</p>
+            </div>
+            <div class="ukm-music">
+              <h2 class="ukm-title">Music.</h2>
+              <p>Yang akan menjadi fokus kami dibidang musik adalah grup band anak muda.</p>
+            </div>
+            <div class="ukm-food">
+              <h2 class="ukm-title">Food.</h2>
+              <p>Yang akan menjadi fokus kami dibidang makanan adalah cemilan kreatif.</p>
+            </div>
+          </div>
+
+          <img src="image/ikonig.png" alt="" width="20%" class="ukm-ig">
+      </div>
+
+    </div>
+  </body>
+</html>
