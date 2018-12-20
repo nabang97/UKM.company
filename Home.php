31b9ff449eb088952f2319717aa0c88710b98188
nabang97 <parawitayolanda@gmail.com>
2018-12-21 01:42:56 +0700

    home

--- Home.php
+++ Home.php
@@ -3,37 +3,112 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="responsive.css">
     <title></title>
   </head>
   <body class="ukm-body">
+    <!-- As a link -->
+<nav class="navbar navbar-light bg-army">
+  <a class="navbar-brand" href="#">
+    <img src="image/logo-ukm.png" width="100" class="img-fluid" alt="">
+  </a>
+  <div class="float-right">
+    <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">SIGN UP <span class="sr-only">(current)</span></a>
+        </li>
+    </ul>
+  </div>
+</nav>
+<div class="container-fluid ukm-slide-container">
+  <div id="demo" class="carousel slide" data-ride="carousel">
 
-    <div class="ukm-container">
-      <div class="ukm-layer"></div>
-      <div class="ukm-content">
-          <img src="image/logo.png" alt="" class="ukm-logo">
-          <div class="ukm-title">
-            <h3>coming soon</h3>
-          </div>
-          <div class="ukm-grid">
-            <div class="ukm-fashion">
-              <h2 class="ukm-title">Fashion.</h2>
-              <p>Yang akan menjadi fokus kami dibidang fashion adalah pedagang hijab.</p>
-            </div>
-            <div class="ukm-music">
-              <h2 class="ukm-title">Music.</h2>
-              <p>Yang akan menjadi fokus kami dibidang musik adalah grup band anak muda.</p>
-            </div>
-            <div class="ukm-food">
-              <h2 class="ukm-title">Food.</h2>
-              <p>Yang akan menjadi fokus kami dibidang makanan adalah cemilan kreatif.</p>
-            </div>
-          </div>
+    <!-- Indicators -->
+    <!-- <ul class="carousel-indicators">
+      <li data-target="#demo" data-slide-to="0" class="active"></li>
+      <li data-target="#demo" data-slide-to="1"></li>
+      <li data-target="#demo" data-slide-to="2"></li>
+    </ul> -->
 
-          <img src="image/ikonig.png" alt="" width="20%" class="ukm-ig">
+    <!-- The slideshow -->
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="image/background.png" alt="Los Angeles" class="slide-image">
+      </div>
+      <!-- <div class="carousel-item">
+        <img src="chicago.jpg" alt="Chicago">
+      </div>
+      <div class="carousel-item">
+        <img src="ny.jpg" alt="New York">
+      </div> -->
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="carousel-control-prev" href="#demo" data-slide="prev">
+      <span class="carousel-control-prev-icon"></span>
+    </a>
+    <a class="carousel-control-next" href="#demo" data-slide="next">
+      <span class="carousel-control-next-icon"></span>
+    </a>
+
+  </div>
+
+</div>
+<div class="container ukm-content-home">
+  <div class="grid-content-home">
+
+      <div class="title-1">
+        <div class="ukm-text-content">
+          <h1 class="ukm-title">about us
+          </h1>
+          <div class="line"></div>
+          <p style="margin-top:10px;">Sebagai anak muda kami ingin berkontribusi memberi manfaat bagi banyak orang melalui kreatifitas dan skill yang kami miliki</p>
+        </div>
+      </div>
+      <div class="text-1">
+          <img src="image/instagram.png" alt="" class="img-fluid">
       </div>
 
-    </div>
+
+      <div class="title-2">
+        <div class="ukm-text-content">
+          <h1 class="ukm-title">how we do?
+          </h1>
+          <div class="line"></div>
+          <p style="margin-top:10px;">Kami berusaha untuk mengkolaborasikan para pengusaha atau pedagang dengan para creator untuk menghasilkan kreatifitas serta memanfaatkan teknologi secara maksimal</p>
+        </div>
+      </div>
+      <div class="text-2">
+        <img src="image\Instagram – 1.png" alt="" class="img-fluid">
+      </div>
+
+
+
+      <div class="title-3">
+        <div class="ukm-text-content">
+          <h1 class="ukm-title">creator
+          </h1>
+          <div class="line"></div>
+          <p style="margin-top:10px;">Creator of Creativity:<br>
+          Graphic Designer<br>Ilustrator <br>Photographer<br>Videographer</p>
+        </div>
+      </div>
+      <div class="text-3">
+        <img src="image\Instagram – 2.png" alt="" class="img-fluid">
+      </div>
+
+  </div>
+</div>
+<div class="container ukm-footer" style="margin-top:100px;margin-bottom:100px;">
+  <center>
+  <img src="image\ukmco-ig.png" alt="" class="img-fluid logo-footer">
+</center>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
   </body>
 </html>
